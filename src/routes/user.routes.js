048f7c145bc3b205a4822f7c2378e8bdef3a01dd
ChayaBasha <johanna.blumenthal@gmail.com>
2020-05-01 15:39:21 -0600

    added put route to update User

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,7 @@ const userRoutes = express.Router();
 
 userRoutes 
   .post('/', userController.registerUser)
-  .post('/update', verifyToken, userController.updateUser) 
+  .put('/update', verifyToken, userController.updateUser) 
   .get('/:userId', userController.getUser)
   .post('/login', userController.login)
   .delete('/:userId', verifyToken, userController.deleteUser)
