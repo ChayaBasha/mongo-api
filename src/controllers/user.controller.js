8b5a98ce50be42d229ffbc0f566634778770d1b6
ChayaBasha <johanna.blumenthal@gmail.com>
2020-05-01 22:24:57 -0600

    fixed user controller to encrypt updated passwords

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,78 +1,84 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
-const {createToken, findByUserCredentials} = require('../services/auth.service');
+const { createToken, findByUserCredentials } = require('../services/auth.service');
 
 const expiresIn = 3600; // I made this one hour because most websites timeout sooner; also made it a const for easier reuse and changing in future
 
-exports.registerUser = async function(req, res) {
+exports.registerUser = async function (req, res) {
   const encryptPassword = await bcrypt.hash(req.body.password, 8);
-  const newUser = new User({...(req.body), password:encryptPassword});
+  const newUser = new User({ ...(req.body), password: encryptPassword });
   const accessToken = await createToken(newUser, expiresIn);
-  newUser.save(function(err, data) {
+  newUser.save(function (err, data) {
     if (err) {
       res.send(err)
     }
 
     res
-    .header('access_token', accessToken)
-    .json(data);
+      .header('access_token', accessToken)
+      .json(data);
   });
 };
 
-exports.login = async function(req, res) {
-  try{
-  const user = await findByUserCredentials(req.body.userName, req.body.password);
-  if(!user) {
-    return res.status(400).send('Cannot find this user');
+exports.login = async function (req, res) {
+  try {
+    const user = await findByUserCredentials(req.body.userName, req.body.password);
+    if (!user) {
+      return res.status(400).send('Cannot find this user');
+    }
+
+    const accessToken = await createToken(user, expiresIn);
+
+    res
+      .header('access_token', accessToken)
+      .send({
+        auth: true,
+        msg: 'Logged in!',
+        token_type: 'bearer',
+        access_token: accessToken,
+        expires_in: expiresIn
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send('Not a valid login/password');
   }
-  
-  const accessToken = await createToken(user, expiresIn); 
-  
-  res
-  .header('access_token', accessToken)
-  .send({
-    auth: true,
-    msg: 'Logged in!',
-    token_type: 'bearer',
-    access_token: accessToken,
-    expires_in: expiresIn
-  });
-} catch (err) {
-  console.log(err);
-  res.status(400).send('Not a valid login/password');
-}
 };
 
-exports.getUser = function(req, res) {
-  User.findById(req.params.userId, function(err, data) {
+exports.getUser = function (req, res) {
+  User.findById(req.params.userId, function (err, data) {
     if (err) {
       res.send(err);
     } else if (data) {
-      const {userName, birthMonth, birthYear} = data;
-      res.json({userName, birthMonth, birthYear});
+      const { userName, birthMonth, birthYear } = data;
+      res.json({ userName, birthMonth, birthYear });
     } else {
       res.status(400).send("No user")
     }
   });
 };
 
-//TO Do (make sure this allows you to update password with new password hashed)
-exports.updateUser = async function(req, res) {
+
+exports.updateUser = async function (req, res) {
+  if (req.body.password) {
+    req.body.password = await bcrypt.hash(req.body.password, 8);
+  }
+
   User.findOneAndUpdate(
     { _id: req.user._id },
     req.body,
     { new: true },
-    function(err, data) {
+    function (err, data) {
       if (err) {
-        res.send(err);
+        console.log(err);
+        res.status(500).send("there was a problem");
+      } else {
+        res.send('succesfully updated');
       }
-      res.send('succesfully updated');
     }
   );
 };
 
-exports.deleteUser = function(req, res) {
-  User.deleteOne({ _id: req.params.userId }, function(err) {
+exports.deleteUser = function (req, res) {
+  User.deleteOne({ _id: req.params.userId }, function (err) {
     if (err) {
       res.send(err);
     }
@@ -82,12 +88,12 @@ exports.deleteUser = function(req, res) {
 
 exports.logout = async function (req, res) {
   try {
-  req.user.token = req.user.tokens.filter((token) => {
-    return token.token !== req.token;
-  });
-  await req.user.save();
-  res.send({success: true});
-} catch (err) {
-  res.status(500). send(err);
-}
+    req.user.token = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send({ success: true });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
